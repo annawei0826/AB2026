6aefb1733f3d1e76efa95899a46b52199c60ad5c
annawei <annawei@benet.com.tw>
2025-12-02 15:27:00 +0800

    site.base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  site: 'https://events-miball.businesstoday.com.tw/',
+  base: '2026/AB2026/',
   plugins: [vue()],
   resolve: {
     alias: {
